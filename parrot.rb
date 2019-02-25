83948ce769b2550c2b4bf559d20a77a3d60ce33b
Tommi Cox <tommicox@gmail.com>
2019-02-25 17:25:29 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,9 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+# Create method `parrot` 
+# that outputs a given phrase
+# and returns the phrase
 
 def parrot(phrase)
+  puts "Polly wants a cracker"
   return phrase
 end
 
